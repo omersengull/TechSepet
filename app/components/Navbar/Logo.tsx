06f511e839fd97798023418136725abce9d4293e
omersengull <omersengul061@hotmail.com>
2024-12-30 08:27:24 +0300

    güncellendi

--- app/components/Navbar/Logo.tsx
+++ app/components/Navbar/Logo.tsx
@@ -6,7 +6,7 @@ const Logo = () => {
     const router=useRouter()
     return (
 
-        <div onClick={()=> router.push("/")} className="text-white font-raleway cursor-pointer"><span className="text-2xl sm:text-3xl">TechSepet</span> <span className="text-sm">.com</span></div>
+        <div onClick={()=> router.push("/")} className="text-white font-raleway cursor-pointer"><span className="text-2xl sm:text-3xl">TechSepet</span></div>
     )
 }
 
