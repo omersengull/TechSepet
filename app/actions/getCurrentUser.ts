f0a6b1b652d98a6be4cadf42d5f5d07fc9d23a6e
omersengull <omersengul061@hotmail.com>
2025-03-24 04:06:50 +0300

    a

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -63,7 +63,7 @@ export async function getCurrentUser() {
             createdAt: currentUser.createdAt?.toISOString(),
             image:currentUser.image?.toString() || null,
             updatedAt: currentUser.updatedAt?.toISOString(),
-            emailVerified: currentUser.emailVerified?.toISOString() || null,
+            emailVerified: currentUser.emailVerified || null,
         };
     } catch (error) {
         console.error("Kullanıcı alınamadı:", error);
