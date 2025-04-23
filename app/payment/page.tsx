0b5694aeaaf2961d9365853537ea3f3af99a1f89
omersengull <omersengul061@hotmail.com>
2025-04-23 16:32:35 +0300

    a

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -3,15 +3,20 @@ import React from 'react'
 import { SessionProvider } from 'next-auth/react'
 import { Elements } from '@stripe/react-stripe-js'
 import { loadStripe } from '@stripe/stripe-js'
+
+import { BrowserRouter } from "react-router-dom";
 import FormPayment from '../formPayment'
 const page = () => {
-    
+
     const stripePromise = loadStripe("pk_test_51R68bCLFXUo4EH8bri3AyKUwFd21mcn2ecRjwP9Q0csZHeXZbA9dh5Mg77pkc17ypzrSBd5SX9OE24a1uaLGcvO100TpnRPk5N");
     return (
         <SessionProvider>
-            <Elements stripe={stripePromise}>
-            <FormPayment />
-        </Elements>
+            <BrowserRouter>
+                <Elements stripe={stripePromise}>
+                    <FormPayment />
+
+                </Elements>
+            </BrowserRouter>
         </SessionProvider >
     )
 }
